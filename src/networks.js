d66f956f8d044c465ea357c34ccd8cf6961b9dcb
harzo <harzojunior@gmail.com>
2020-07-01 15:21:55 +0200

    Update base58 prefixes

--- src/networks.js
+++ src/networks.js
@@ -2,13 +2,13 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.bitcoin = {
   messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'royale',
+  bech32: 'bc',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4,
   },
-  pubKeyHash: 78,
-  scriptHash: 60,
+  pubKeyHash: 0x00,
+  scriptHash: 0x05,
   wif: 0x80,
 };
 exports.regtest = {
